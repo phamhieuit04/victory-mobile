d6b93dc02dc124290f850bc52626380cef2eed47
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-11-04 14:22:07 +0700

    Add base project structure

---
+++ app/src/main/java/com/example/victorymobile/App.kt
@@ -0,0 +1,9 @@
+package com.example.victorymobile
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AppScreen(modifier: Modifier = Modifier) {
+    
+}
